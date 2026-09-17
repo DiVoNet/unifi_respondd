297f15f072ba3986b33708f1449ac1776303e821
Lukas Mittermeier <lukas.mittermeier@lmsound.de>
2026-09-17 23:09:43 +0200

    Add timeout to avoid indefinite hang

--- unifi_respondd/unifi_client.py
+++ unifi_respondd/unifi_client.py
@@ -134,7 +134,7 @@ def get_location_by_address(address, app):
 def scrape(url):
     """returns remote json"""
     try:
-        return rget(url).json()
+        return rget(url, timeout=60).json()
     except Exception as ex:
         logger.error("Error: %s" % (ex))
 
